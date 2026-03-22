8e87c2dc81db9bf9dd77458a58b1fb0449a5aa6a
DeiversonMAP <73710805+DeiversonMAP@users.noreply.github.com>
2026-03-22 12:37:26 -0300

    Removendo pasta game da raiz

--- game/src/elements/player/types.tsx
+++
@@ -1,8 +0,0 @@
-export type PlayerProps = {
-    scale?: number;
-    gridPosition: [number, number];
-    rotationIndex: number;
-    blockHeight: number;
-    stepIndex?: number;
-    command?: string;
-};
